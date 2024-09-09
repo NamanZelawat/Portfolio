2a42f2c49c723b23217a6e357f387868bcae5c39
NamanZelawat <namanzelawat@gmail.com>
2024-09-09 23:00:56 +0900

    Content and video setup.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Portfolio baby let's go.</div>;
+  return (
+    // <div className="app">
+    //   <div className="text-wrapper">
+    //     <h1>Naman Zelawat</h1>
+    //     <h4>Links.</h4>
+    //     <button>Do not click!!</button>
+    //   </div>
+    // </div>
+    <div className="video-container">
+      <video
+        className="full-screen-video"
+        src={`${process.env.PUBLIC_URL}/test.mp4`}
+        loop
+        muted
+        autoPlay
+      ></video>
+    </div>
+  );
 }
 
 export default App;
